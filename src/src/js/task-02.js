6ea72d4f3759a112cd159ad57f00d272797bb367
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-07-28 12:15:36 +0200

    tack-2

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const listIngredients = document.querySelector('#ingredients');
+const itemsIngredientsEl = [];
+
+function createItemEl(arr) {
+
+  arr.map((item) => {
+    const itemEl = document.createElement('li');
+
+      itemEl.classList.add('item');
+      itemEl.textContent = item; 
+      itemsIngredientsEl.push(itemEl)
+    }).join('');
+
+    listIngredients.append(...itemsIngredientsEl)
+};
+
+createItemEl(ingredients);
+
+
+
+
