05ebfaf7500c3a354f0646aaaee376266641be8d
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-07-30 00:13:37 +0200

    update readme

--- src/src/js/task-10.js
+++ src/src/js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controlsEl = {
+  inputEl : document.querySelector('input'),
+  btnCreate : document.querySelector('[data-create]'),
+  btnDelete : document.querySelector('[data-destroy]'),
+  
+}
+
+
+controlsEl.inputEl.addEventListener('input', onInputElInput);
+
+const quantity = onInputElInput (); 
+
+function onInputElInput (evt) {
+  return parseInt(evt.currentTarget.value);
+  
+}
+console.log(quantity)
+
+// controlsEl.btnCreate.addEventListener('click', onBtnCreateClick);
+
+// function onBtnCreateClick (evt) {
+//   createBoxes(onInputElInput())
+// }
+
+// function createBoxes(amount) {
+// const createDivEl = document.createElement('div');
+// console.log(createDivEl);
+// };
+
