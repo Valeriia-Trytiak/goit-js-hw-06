199c0ff44d747eeb65c96df0350d4021f4ca5a79
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-07-29 23:22:28 +0200

    task-8

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormElSubmit);
+
+function onFormElSubmit (evt) {
+// забороняю перезавантаження сторінки
+evt.preventDefault();
+
+// збираю властивості
+const {
+    elements: { email, password }
+} = evt.currentTarget;
+
+// перевіряю умову заповнення усіх полів
+if (email.value === "" || password.value === "") {
+alert(`Усі поля повинні бути заповнені!`);
+};
+
+// забираю значення логіну та пороля у консоль при заповнені усіх полів
+if (email.value !== "" && password.value !== "") {
+console.log(`Login: ${email.value}, Password: ${password.value}`)};
+
+// очищую форму
+evt.currentTarget.reset();
+};
