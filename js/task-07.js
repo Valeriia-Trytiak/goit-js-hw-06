1ed1b86b943fdddcc761ea8ec04ad20b02332835
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-07-31 08:04:55 +0200

    update task
    додала коментар-завдання на кожну таску. Підправила 1 та 8

--- js/task-07.js
+++ js/task-07.js
@@ -1,3 +1,5 @@
+/*Напиши скрипт, який реагує на зміну значення input#font-size-control (подія input) і змінює інлайн-стиль span#text, оновлюючи властивість font-size. В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.*/
+
 const inputControlEl = document.querySelector('#font-size-control');
 
 const textEl = document.querySelector('#text');
